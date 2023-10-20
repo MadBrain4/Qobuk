403ae38166436752efde14a9b1ecec66c89c09d6
José Vicente <Josevld412@gmail.com>
2023-10-20 03:18:53 -0400

    CRUD de Users

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::POST('/login', [AuthController::class, 'login']);
 Route::POST('/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function () { 
+    Route::resource('/users', UserController::class);
+});
